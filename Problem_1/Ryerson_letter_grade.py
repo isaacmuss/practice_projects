938ff44d9f693d07424bae3e6ca56de58f3522c2
isaacmuss <isaacmm2019@gmail.com>
2024-06-08 20:45:14 -0600

    Final script

--- Problem_1/Ryerson_letter_grade.py
+++ Problem_1/Ryerson_letter_grade.py
@@ -2,33 +2,31 @@
 # Ryerson letter grade
 
 # Define de function
-pct = 30
-
 def ryerson_letter_grade(pct):
 
     if pct <= 49:
-        return'F'
+        return 'F'
     elif pct <= 52:
-        return'D-'
+        return 'D-'
     elif pct <= 56:
-        return'D'
+        return 'D'
     elif pct <= 59:
-        return'D+'    
+        return 'D+'    
     elif pct <= 62:
-        return'C-'
+        return 'C-'
     elif pct <= 66:
-        return'C'
+        return 'C'
     elif pct <= 69:
-        return'C+'
+        return 'C+'
     elif pct <= 72:
-        return'B-'
+        return 'B-'
     elif pct <= 76:
-        return'B'
+        return 'B'
     elif pct <= 79:
-        return'B+'
+        return 'B+'
     elif pct <= 84:
-        return'A-'
+        return 'A-'
     elif pct <= 89:
-        return'A'
-    elif pct <= 100:
-        return'A+'
+        return 'A'
+    elif pct <= 150:
+        return 'A+'
