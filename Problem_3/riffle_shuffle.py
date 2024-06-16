bb2fb353270689b716356be1d6f912f2cbd42fc9
isaacmuss <isaacmm2019@gmail.com>
2024-06-16 11:57:27 -0600

    Second try

--- Problem_3/riffle_shuffle.py
+++ Problem_3/riffle_shuffle.py
@@ -1,28 +1,18 @@
-
 items = [1, 2, 3, 4, 5, 6, 7, 8]
 
-items_0 = items[:int((len(items))/2)]
+items_dict = dict()
 
-items_1 = items[int((len(items)/2)):]
+for num, val in enumerate(items):
+    items_dict[num] = val
 
-indic = True 
+size = int(len(items_dict))
+
+rango_1 = list(range(0,int(size/2)))
+
+rango_2 = list(range(int(size/2),size))
 
 new = []
 
-for num, val in enumerate(items):
-    print(num + 1, val)
-    new.append(val)
-    if num in range(int(len(items)/2),int(len(items))):
-        new.append(val)
-
-
-
-# for num in items_0:
-#     for num_2 in items_1:
-#         new.append(num)
-#         new.append(num_2)
-#         continue
-#     continue
-
-print(new)
-# shuffle = [(i) for i in range(items)]
+for i in range(int(size/2)):
+    new.append(items_dict[rango_1[i]])    
+    new.append(items_dict[rango_2[i]])
