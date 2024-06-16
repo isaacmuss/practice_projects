71c0b6a873f97b6b0ce0da2faac66cd843f5650e
isaacmuss <isaacmm2019@gmail.com>
2024-06-16 12:31:10 -0600

    Final version

--- Problem_4/even_the_odds.py
+++ Problem_4/even_the_odds.py
@@ -1,14 +1,20 @@
-n = 1357913579135798
+def only_odd_digits(n):
+    ''' Define even the odds function '''
 
-n_str = str(n)
+    # Check if n is a positive integer
+    if n < 0 or type(n) is float:
+        raise ValueError('Only positive integers are allowed')
 
-for i in range(len(n_str)):
-    digit = int(n_str[i])
-    if digit % 2 == 0:
-        verif = False
-        break
-    else:
-        verif = True
+    # Convert n to str
+    n_str = str(n)
 
-print(verif)
+    # Loop through every digit
+    for i in range(len(n_str)):
+        digit = int(n_str[i]) # Extract i digit
+        if digit % 2 == 0: # Check if digit is even
+            verif = False # Assign False, even digit was found
+            break # An even digit was found, cease evaluation
+        else:
+            verif = True # Assign True, only odd number
 
+    return verif # Return result
